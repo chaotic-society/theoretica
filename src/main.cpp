988b3d6f6f096d658f19a7212fdb3204c0a76e5c
Mattia Isgrò <matth_590@yahoo.it>
2021-04-18 16:33:15 +0200

    Add new statistics functions (least squares, TSS, Gaussian distribution ...)

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,27 @@ using namespace umath;
 
 int main(int argc, char const *argv[]) {
 
+	// Quaternion rotation of a vector
 	vec3 v = {1, 2, 3};
 	print_vec(v);
 	print_vec(quat::rotate(v, PI, {0, 1, 0}));
 
-	quat q = {1, 0, 0, 1};
-
+	// Complex multiplication and rotation
 	complex z = {1, 0};
 	complex w = {0, 1};
-	print_complex_alg(z);
-	print_complex_alg(w);
-	print_complex_alg(z * w);
+	complex r = complex::rotor(PI);
+	print_complex(z);
+	print_complex(w);
+	print_complex(z * w);
+	print_complex(z * r);
 
-	vec4 vec1 = {1, 3, 1, 2};
-	vec4 vec2 = {-1, 2, 1, 0};
+	// Dot product and cross product between vectors
+	vec3 v1 = {1, 3, 1};
+	vec3 v2 = {-1, 2, 1};
+	std::cout << v1 * v2 << std::endl;
+	print_vec(v1.cross(v2));
 
+	// Matrix product and transposition
 	auto m1 = mat4(2);
 	auto m2 = mat<3, 4>(3);
 	m1.at(1, 2) = 3;
@@ -30,14 +36,19 @@ int main(int argc, char const *argv[]) {
 
 	print_mat(mat4::identity() * mat4::diagonal(2.0));
 
-	vec_buff X = {1, 1.2, 0.9};
-	vec_buff Y = {10.1, 11.7, 9.2};
+	// Statistical analysis
+	vec_buff X = {1, 1.2, 0.9, 0.78, 0.71};
+	vec_buff Y = {10.1, 11.7, 9.2, 8.1, 6.9};
 
-	std::cout << covariance(X, Y) << std::endl;
-	std::cout << sample_standard_deviation(X) << std::endl;
-	std::cout << sample_standard_deviation(Y) << std::endl;
-	std::cout << mean_standard_deviation(X) << std::endl;
-	std::cout << mean_standard_deviation(Y) << std::endl;
+	std::cout << sample_covariance(X, Y) << std::endl;
+	std::cout << smpl_stdev(X) << std::endl;
+	std::cout << sample_standard_relative_error(X) << std::endl;
+	std::cout << smpl_stdev(Y) << std::endl;
+	std::cout << sample_standard_relative_error(Y) << std::endl;
+	std::cout << smpl_stdom(X) << std::endl;
+	std::cout << smpl_stdom(Y) << std::endl;
+	std::cout << lst_sqrs_lin_intercept(X, Y) << std::endl;
+	std::cout << lst_sqrs_lin_slope(X, Y) << std::endl;
 
 	return 0;
 }
