ed6fac8f2d02f0ed4de36d2bee7ab9d6b7b2e0c2
Mattia Isgrò <matth_590@yahoo.it>
2021-04-18 16:34:29 +0200

    Add support for 2 data records

--- tools/labcalc.cpp
+++ tools/labcalc.cpp
@@ -6,32 +6,62 @@
 
 using namespace uroboro;
 
+
 int main(int argc, char const *argv[]) {
 
-	std::cout << "Insert each value and press Enter, write END to stop insertion" << std::endl;
-
-	vec_buff X;
+	std::cout << "How many records to insert? (1 or 2 only)" << std::endl;
 	std::string line;
-	real value;
+	std::getline(std::cin, line);
 
-	while(true) {
+	bool two_records;
 
-		std::getline(std::cin, line);
-
-		if(line == "END")
-			break;
-
-		try {
-			value = std::stod(line);
-		} catch(...) {
-			std::cout << "Input conversion error" << std::endl;
-		}
-
-		X.emplace_back(value);
-
+	if(line == "1")
+		two_records = false;
+	else if(line == "2")
+		two_records	= true;
+	else {
+		std::cout << "Insertion error" << std::endl;
+		exit(1);
 	}
 
+	std::cout << "\nInsert each value and press Enter (END to stop insertion)" << std::endl;
+
+	vec_buff X;
+	vec_buff Y;
+
+	std::cout << "Insert X values:" << std::endl;
+	insert_data(X, "");
+
+	if(two_records) {
+		std::cout << "Insert Y values:" << std::endl;
+		insert_data(Y, "");
+	}
+
+	std::cout << "\nStats for X:" << std::endl;
 	print_sample_stats(X);
+
+	if(two_records) {
+		std::cout << "\nStats for Y:" << std::endl;
+		print_sample_stats(Y);
+	}
+
+	if(two_records) {
+
+		std::cout << "Covariance: " << sample_covariance(X, Y) << "\n\n";
+
+		real intercept = least_squares_linear_intercept(X, Y);
+		real slope = least_squares_linear_slope(X, Y);
+
+		std::cout << "Minimum Squares Linearization:\n";
+		std::cout << "Minimum Squares Intercept: " << intercept << std::endl;
+		std::cout << "Minimum Squares Slope: " << slope << std::endl;
+		std::cout << "Minimum Squares Error: " <<
+			least_squares_linear_error(X, Y, intercept, slope) << std::endl;
+
+		std::cout << "Correlation Coefficient: " <<
+			sample_correlation_coefficient(X, Y) << std::endl;
+		std::cout << std::endl;
+	}
 
 	std::cout << "Press Enter to exit..." << std::endl;
 	std::cin.get();
