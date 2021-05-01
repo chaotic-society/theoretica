7e3cd60ff88e18cf3d96b9c24a6b3038f179b511
Mattia Isgrò <matth_590@yahoo.it>
2021-05-01 15:09:33 +0200

    Add quotient_sum function

--- src/vec_buff.h
+++ src/vec_buff.h
@@ -10,67 +10,78 @@ namespace uroboro {
 	using vec_buff = std::vector<real>;
 
 
-    // Sum the products of two sets of values
+	// Sum the products of two sets of values
 	inline real product_sum(const vec_buff& X, const vec_buff& Y) {
 
-        if(X.size() != Y.size())
-            // throw...
-            return 0;
+		if(X.size() != Y.size())
+			// throw...
+			return 0;
 
-        real res = 0;
-        for(int i = 0; i < X.size(); i++) {
-            res += X[i] * Y[i];
-        }
+		real res = 0;
+		for(int i = 0; i < X.size(); i++)
+			res += X[i] * Y[i];
 
-        return res;
+		return res;
 	}
 
 
     // Sum the products of three sets of values
     inline real product_sum(const vec_buff& X, const vec_buff& Y, const vec_buff& Z) {
 
-        if(X.size() != Y.size() || X.size() != Z.size())
-            // throw...
-            return 0;
+		if(X.size() != Y.size() || X.size() != Z.size())
+			// throw...
+			return 0;
 
-        real res = 0;
-        for(int i = 0; i < X.size(); i++) {
-            res += X[i] * Y[i] * Z[i];
-        }
+		real res = 0;
+		for(int i = 0; i < X.size(); i++)
+			res += X[i] * Y[i] * Z[i];
 
-        return res;
+		return res;
     }
+
+
+    // Sum the quotients of two sets of values
+	inline real quotient_sum(const vec_buff& X, const vec_buff& Y) {
+
+		if(X.size() != Y.size())
+			// throw...
+			return 0;
+
+		real res = 0;
+		for(int i = 0; i < X.size(); i++)
+			res += X[i] / Y[i];
+
+		return res;
+	}
 
 
     // Sum the squares of a set of values
     inline real sum_squares(const vec_buff& X) {
 
-        if(!X.size())
-            // throw...
-            return 0;
+		if(!X.size())
+			// throw...
+			return 0;
 
-        real res = 0;
-        for(int i = 0; i < X.size(); i++) {
-            res += X[i] * X[i];
-        }
+		real res = 0;
+		for(int i = 0; i < X.size(); i++)
+			res += X[i] * X[i];
 
-        return res;
+		return res;
     }
 
 
     // Sum together a set of values
 	inline real sum(const vec_buff& X) {
 
-        if(!X.size())
-            // throw...
-            return 0;
+		if(!X.size())
+			// throw...
+			return 0;
 
-        real res = 0;
-        for(int i = 0; i < X.size(); i++) {
-            res += X[i];
-        }
+		real res = 0;
+		for(int i = 0; i < X.size(); i++)
+			res += X[i];
 
-        return res;
+		return res;
 	}
 
 }
