5d8d15389c7cb5f2958a91cb54d1e8b3ba7a09e4
Mattia Isgrò <mattiaisgro@users.noreply.github.com>
2021-12-22 23:17:29 +0100

    Improve tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,3 @@
-#define UROBORO_INCLUDE_ALL
 #include "../src/uroboro.h"
 
 #include <cmath>
@@ -13,8 +12,8 @@ unsigned int total_errors = 0;
 
 unsigned int tolr_test_runs = 0;
 unsigned int curr_errors = 0;
-real cum_err = 0;
-real max_err = 0;
+long double cum_err = 0;
+long double max_err = 0;
 
 void test_start(std::string f) {
 
@@ -30,63 +29,63 @@ void test_start(std::string f) {
 
 void test_end() {
 
-	std::cout << "Finished testing " << func_name << std::endl;
+	std::cout << "\nFinished testing " << func_name << std::endl;
 
 	if(curr_errors == 0) {
 		std::cout << "All tests passed successfully" << std::endl;
 	} else {
-		std::cout << curr_errors << " tests failed" << std::endl;
+		std::cout << curr_errors << " tests failed (" <<
+		(curr_errors / (real) tolr_test_runs * 100) << "%)" << std::endl;
 	}
 
-	std::cout << "Absolute Error: " << cum_err << std::endl;
 	std::cout << "Mean Error: " << (cum_err / (real) tolr_test_runs) << std::endl;
-	std::cout << "Maximum Error: " << max_err << "\n" << std::endl;
+	std::cout << "Maximum Error: " << max_err << "\n\n" << std::endl;
 	func_name = "";
 }
 
 
 template<typename T>
-bool good_enough(T a, T b) {
-	return umath::abs(b - a) < TOLERANCE;
+bool good_enough(T a, T b, real tolerance = TOLERANCE) {
+	return umath::abs(b - a) < tolerance;
 }
 
 
 template<typename T1, typename T2>
-void test_tol(T1 evaluated, T1 expected, T2 input, bool silent = false) {
+void test_tol(T1 evaluated, T1 expected, T2 input, real tolerance = TOLERANCE, bool silent = false) {
 
 	real diff = umath::abs(evaluated - expected);
 	cum_err += diff;
 	max_err = umath::max(diff, max_err);
 	tolr_test_runs++;
 
-	if(!good_enough(evaluated, expected)) {
+	if(!good_enough(evaluated, expected, tolerance)) {
 
 		if(!silent) {
-			std::cout << "Test failed on " << func_name << ":" << std::endl;
-			std::cout << "\tExpected: " << expected << std::endl;
-			std::cout << "\tEvaluated: " << evaluated << std::endl;
-			std::cout << "\tInput: " << input << std::endl;
-			std::cout << "\tDiff: " << diff << std::endl;
+			std::cout << "\tTest failed on " << func_name << ":" << std::endl;
+			std::cout << "\t\tExpected: " << expected << std::endl;
+			std::cout << "\t\tEvaluated: " << evaluated << std::endl;
+			std::cout << "\t\tInput: " << input << std::endl;
+			std::cout << "\t\tDiff: " << diff << std::endl;
 		}
 		
 		total_errors++;
 		curr_errors++;
 
 	} else if(!silent) {
-		std::cout << "Test passed with diff: " << diff << std::endl;
+		std::cout << "\tTest passed with diff: " << diff << std::endl;
 	}
 }
 
 
-void test_tolr(real evaluated, real expected, real input, bool silent = false) {
-	test_tol<real, real>(evaluated, expected, input, silent);
+void test_tolr(real evaluated, real expected, real input, real tolerance = TOLERANCE, bool silent = false) {
+	test_tol<real, real>(evaluated, expected, input, tolerance, silent);
 }
 
 
-void test_tolr_interval(real_function f, real_function f_exp, real a, real b, unsigned int steps = 1000) {
+void test_tolr_interval(real_function f, real_function f_exp, real a, real b,
+	real tolerance = TOLERANCE, unsigned int steps = 1000) {
 
-	std::cout << "Testing " << func_name <<
-		" on interval [" << a << ", " << b << "] with " << steps << " steps" << std::endl;
+	std::cout << "\tTesting on interval [" << a << ", " << b << "] with " << steps << " steps" << std::endl;
 
 	real dx = (b - a) / (real) steps;
 
@@ -94,7 +93,7 @@ void test_tolr_interval(real_function f, real_function f_exp, real a, real b, un
 
 		real x = a + i * dx;
 
-		test_tolr(f(x), f_exp(x), x, true);
+		test_tolr(f(x), f_exp(x), x, tolerance, true);
 	}
 }
 
@@ -126,6 +125,8 @@ int main(int argc, char const *argv[]) {
 	test_start("umath::ln(real)");
 
 		test_tolr(umath::ln(E), 1, E);
+		test_tolr_interval(umath::ln, std::log, 0.00000001, 1);
+		test_tolr_interval(umath::ln, std::log, 0.00000001, 0.000001);
 		test_tolr_interval(umath::ln, std::log, 0.0001, 1000);
 
 	test_end();
@@ -136,6 +137,8 @@ int main(int argc, char const *argv[]) {
 		test_tolr(umath::log2(2), 1, 2);
 		test_tolr(umath::log2(4), 2, 4);
 		test_tolr(umath::log2(8), 3, 8);
+		test_tolr_interval(umath::log2, std::log2, 0.00000001, 1);
+		test_tolr_interval(umath::log2, std::log2, 0.0001, 1000);
 
 	test_end();
 
@@ -145,30 +148,42 @@ int main(int argc, char const *argv[]) {
 		test_tolr(umath::log10(10), 1.f, 10);
 		test_tolr(umath::log10(100), 2.f, 100);
 		test_tolr(umath::log10(1000), 3.f, 1000);
+		test_tolr_interval(umath::log10, std::log10, 0.00000001, 1);
+		test_tolr_interval(umath::log10, std::log10, 0.0001, 1000);
 
 	test_end();
 
 
 	test_start("umath::exp");
 
-		test_tolr(umath::exp(2), umath::E * umath::E, 2);
-		test_tolr(umath::exp(1), umath::E, 1);
+		test_tolr(umath::exp(2), E * E, 2);
+		test_tolr(umath::exp(1), E, 1);
+		// test_tolr_interval(umath::exp, std::exp, 0.00000001, 1);
+		// test_tolr_interval(umath::exp, std::exp, 1, 10);
 
 	test_end();
 
 
 	test_start("umath::powf_approx");
 
-		test_tolr(umath::powf_approx(2.f, 0.5f), umath::SQRT2, 2);
+		test_tolr(umath::powf_approx(2.0, 0.5), SQRT2, 2);
+		test_tolr(umath::powf_approx(2, 2), 4, 2);
+		test_tolr(umath::powf_approx(3, 2), 9, 3);
 
 	test_end();
 
 
 	test_start("umath::sin");
 
-		// sin, cos and tan are precise
 		test_tolr(umath::sin(0.5f), 0.4794255386, 0.5);
 		test_tolr(umath::sin(3), 0.14112000806, 3);
+		test_tolr_interval(umath::sin, std::sin, 0, 2 * PI, 100000);
+		test_tolr_interval(umath::sin, std::sin, 0, 10 * PI);
+		test_tolr_interval(umath::sin, std::sin, -10 * PI, 0);
+		test_tolr_interval(umath::sin, std::sin, 0, 100 * PI);
+		test_tolr_interval(umath::sin, std::sin, -100 * PI, 0);
+		test_tolr_interval(umath::sin, std::sin, 0, 1000 * PI);
+		test_tolr_interval(umath::sin, std::sin, -1000 * PI, 0);
 
 	test_end();
 
@@ -177,6 +192,13 @@ int main(int argc, char const *argv[]) {
 
 		test_tolr(umath::cos(0.5f), 0.87758256189, 0.5);
 		test_tolr(umath::cos(3), -0.9899924966, 3);
+		test_tolr_interval(umath::cos, std::cos, 0, 2 * PI, 100000);
+		test_tolr_interval(umath::cos, std::cos, 0, 10 * PI);
+		test_tolr_interval(umath::cos, std::cos, -10 * PI, 0);
+		test_tolr_interval(umath::cos, std::cos, 0, 100 * PI);
+		test_tolr_interval(umath::cos, std::cos, -100 * PI, 0);
+		test_tolr_interval(umath::cos, std::cos, 0, 1000 * PI);
+		test_tolr_interval(umath::cos, std::cos, -1000 * PI, 0);
 
 	test_end();
 
@@ -185,34 +207,40 @@ int main(int argc, char const *argv[]) {
 
 		test_tolr(umath::tan(0.5f), 0.54630248984, 0.5);
 		test_tolr(umath::tan(3), -0.14254654307, 3);
+		test_tolr_interval(umath::tan, std::tan, 0, 2 * PI, 100000);
+		test_tolr_interval(umath::tan, std::tan, 0, 10 * PI);
+		test_tolr_interval(umath::tan, std::tan, -10 * PI, 0);
+		test_tolr_interval(umath::tan, std::tan, 0, 100 * PI);
+		test_tolr_interval(umath::tan, std::tan, -100 * PI, 0);
+		test_tolr_interval(umath::tan, std::tan, 0, 1000 * PI);
+		test_tolr_interval(umath::tan, std::tan, -1000 * PI, 0);
 
 	test_end();
 
 
-	// test_start(umath::asin);
+	// test_start("umath::asin(real)");
 
-	// 	// After 0.9 gets a lot less precise
-	// 	test_tolr(umath::asin(0.5), 0.5235987756, 0.5);
-	// 	test_tolr(umath::asin(0.9), 1.119769515, 0.9);
-
-	// test_end();
-
-
-	// test_start(umath::acos);
-
-	// 	// After 0.9 gets less precise
-	// 	test_tolr(umath::acos(0.5), 1.0471975512, 0.5);
-	// 	test_tolr(umath::acos(0.9), 0.4510268118, 0.9);
+	// 	test_tolr_interval(umath::asin, std::asin, 0, 1, 0.0001);
+	// 	test_tolr_interval(umath::asin, std::asin, -1, 0, 0.0001);
 
 	// test_end();
 
 
-	// test_start("atan(real)");
+	// test_start("umath::acos(real)");
 
-	// 	// atan is really imprecise
-	// 	test_tolr(umath::atan(0.5), 0.54630248984, 0.5);
-	// 	test_tolr(umath::atan(0.9), 0.78037308007, 0.9);
-	// 	test_tolr_interval(umath::sqrt, std::atan, 0, 1);
+	// 	test_tolr_interval(umath::acos, std::acos, 0, 1, 0.0001);
+	// 	test_tolr_interval(umath::acos, std::acos, -1, 0, 0.0001);
+
+	// test_end();
+
+
+	// test_start("umath::atan(real)");
+
+	// 	// Allow greater tolerance on atan
+	// 	test_tolr(umath::atan(0.5), std::atan(0.5), 0.5, 0.0001);
+	// 	test_tolr(umath::atan(0.9), std::atan(0.9), 0.9, 0.0001);
+	// 	test_tolr_interval(umath::atan, std::atan, 0, 1, 0.0001);
+	// 	test_tolr_interval(umath::atan, std::atan, -1, 0, 0.0001);
 
 	// test_end();
 
@@ -234,23 +262,9 @@ int main(int argc, char const *argv[]) {
 
 	test_end();
 
+	if(total_errors == 0)
+		std::cout << "All tests on all functions and modules successfully passed\n" << std::endl;
 
-	// Trick test framework with fake function
-	// test_start(vec3_misc);
-
-	// 	umath::vec3 v3 = {10, 15, 20};
-	// 	umath::vec4 v4 = {1, 2, 3, 4};
-	// 	umath::mat4 m = umath::mat4::diagonal(2);
-
-	// 	v4 = m * v4;
-
-	// 	test_tolr(v4[0], 2);
-
-	// 	v3.normalize();
-
-	// 	test_tolr(v3.magnitude(), 1);
-
-	// test_end();
 
 	return total_errors;
 }
