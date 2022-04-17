8d0121e54df72a63e070bd4e7c48db6097241ef4
Mattia Isgrò <mattiaisgro@users.noreply.github.com>
2022-04-17 17:07:03 +0200

    Update error propagation example

--- examples/error_propagation.cpp
+++ examples/error_propagation.cpp
@@ -1,10 +1,32 @@
 
 ///
-/// @file error_propagation.h Automatic error propagation
+/// @file error_propagation.cpp Automatic error propagation using automatic differentiation
 ///
 
 #include "uroboro.h"
 using namespace umath;
+
+
+template<unsigned int N>
+real product_sum(vec<N> g, vec<N> v) {
+
+	real res = 0;
+	for (int i = 0; i < N; ++i)
+		res += abs(g.at(i) * v.at(i));
+
+	return res;
+}
+
+template<unsigned int N>
+real product_sum_quad(vec<N> g, vec<N> v) {
+
+	real res = 0;
+	for (int i = 0; i < N; ++i)
+		res += square(g.at(i) * v.at(i));
+
+	return umath::sqrt(res);
+}
+
 
 
 // Example function to compute error on
@@ -18,11 +40,13 @@ NumType theta(vec<2, NumType> v) {
 }
 
 
+
 int main(int argc, char const *argv[]) {
 
 	real r, d;
-	real delta_r, delta_d_lf;
+	real delta_r, delta_d;
 
+	// Read data and errors from stdin
 	std::cout << "r = ";
 	std::cin >> r;
 
@@ -32,26 +56,21 @@ int main(int argc, char const *argv[]) {
 	std::cout << "d = ";
 	std::cin >> d;
 
-	std::cout << "delta_d_lf = ";
-	std::cin >> delta_d_lf;
+	std::cout << "delta_d = ";
+	std::cin >> delta_d;
 
+	// Construct data and error as vectors
 	vec2 data = {r, d};
-	vec2 err = {delta_r, delta_d_lf};
+	vec2 err = {delta_r, delta_d};
 
-	auto grad = gradient(theta, data);
-	real err_tot = 0;
+	// Compute the partial derivatives of the function
+	vec2 gradient = umath::gradient(theta, data);
 
-	// Sum under quadrature
-	for (int i = 0; i < data.size(); ++i)
-		err_tot += square(grad.at(i)) * square(err.at(i));
+	std::cout << "Total error (direct sum): " <<
+		product_sum(gradient, err) << std::endl;
 
-	err_tot = umath::sqrt(err_tot);
-
-	// Linear sum
-	// for (int i = 0; i < data.size(); ++i)
-	// 	err_tot += grad.at(i) * err.at(i);
-
-	std::cout << "Total error:\n" << err_tot << std::endl;
+	std::cout << "Total error (sum under quadrature): " <<
+		product_sum_quad(gradient, err) << std::endl;
 
 	return 0;
 }
