7d8c90a598097408211fe07811e16c692526b9bb
Mattia Isgrò <mattiaisgro@users.noreply.github.com>
2022-06-04 18:23:54 +0200

    Small improvements

--- examples/dist_sample.cpp
+++ examples/dist_sample.cpp
@@ -25,14 +25,30 @@ int main(int argc, char const *argv[]) {
 	PRNG g = PRNG::xoshiro(time(nullptr));
 
 	// Discard the first 10000 values
+	// to get better results
 	g.discard(10000);
 
 	// Generate 1000 values
 	for (int i = 0; i < 1000; ++i) {
+
+		// Generate a random variable following a uniform distribution
+		// in the interval [0, 1]
 		f_uniform << rand_real(0, 1, g) << ",\n";
+		
+		// Generate a random variable following a gaussian distribution
+		// with mean = 0 and standard deviation = 1
 		f_gaussian << rand_gaussian(0, 1, g) << ",\n";
+
+		// Generate a random variable following an exponential distribution
+		// with rate = 1
 		f_exponential << rand_exponential(1, g) << ",\n";
+
+		// Generate a random variable following a Cauchy distribution
+		// with location 0 and scale 1
 		f_cauchy << rand_cauchy(0, 1, g) << ",\n";
+		
+		// Generate a random variable following a Pareto distribution
+		// with scale = 1 and shape = 2
 		f_pareto << rand_pareto(1, 2, g) << ",\n";
 	}
 
