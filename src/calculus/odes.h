e215796a95a92f769df79e459857710521e8d673
Mattia Isgrò <mattiaisgro@users.noreply.github.com>
2022-06-22 15:23:51 +0200

    Add Adams-Bashforth linear multistep methods

--- src/calculus/odes.h
+++ src/calculus/odes.h
@@ -17,15 +17,14 @@ namespace theoretica {
 	/// The current state of an ODE integration
 	/// for an N dimensional differential equation.
 	template<unsigned int N>
-	class ode_state {
-		public:
+	struct ode_state {
+
 			real t;
 			vec<N> y;
 
-			ode_state(real t, vec<N> y) {
-				this->t = t;
-				this->y = y;
-			}
+			ode_state() : t(0), y(0) {}
+
+			ode_state(real t, vec<N> y) : t(t), y(y) {}
 	};
 
 
@@ -33,20 +32,22 @@ namespace theoretica {
 	/// The current state of an ODE integration
 	/// for a differential equation in one unknown.
 	template<>
-	class ode_state<1> {
-	public:
+	struct ode_state<1> {
+
 			real t;
 			real y;
 
-			ode_state(real t, real y) {
-				this->t = t;
-				this->y = y;
-			}
+			ode_state() : t(0), y(0) {}
+
+			ode_state(real t, real y) : t(t), y(y) {}
 	};
 
 
 	/// Integrate numerically a differential equation in one unknown
 	/// using Euler's method.
+	/// @param f A function which computes the derivative of the state
+	/// @param s The current state of the ODE
+	/// @param h The real step size
 	ode_state<1> integrate_ode_euler(real(*f)(real, real), ode_state<1> s, real h) {
 
 		return ode_state<1>(s.t + h, s.y + h * f(s.t, s.y));
@@ -55,6 +56,9 @@ namespace theoretica {
 
 	/// Integrate numerically a differential equation in N unknowns
 	/// using Euler's method.
+	/// @param f A function which computes the derivative of the state
+	/// @param s The current state of the ODE
+	/// @param h The real step size
 	template<unsigned int N>
 	ode_state<N> integrate_ode_euler(vec<N>(*f)(real, vec<N>), ode_state<N> s, real h) {
 
@@ -64,6 +68,9 @@ namespace theoretica {
 
 	/// Integrate numerically a differential equation in one unknown
 	/// using the midpoint method.
+	/// @param f A function which computes the derivative of the state
+	/// @param s The current state of the ODE
+	/// @param h The real step size
 	ode_state<1> integrate_ode_midpoint(real(*f)(real, real), ode_state<1> s, real h) {
 
 		return ode_state<1>(
@@ -74,6 +81,9 @@ namespace theoretica {
 
 	/// Integrate numerically a differential equation in N unknowns
 	/// using the midpoint method.
+	/// @param f A function which computes the derivative of the state
+	/// @param s The current state of the ODE
+	/// @param h The real step size
 	template<unsigned int N>
 	ode_state<N> integrate_ode_midpoint(vec<N>(*f)(real, vec<N>), ode_state<N> s, real h) {
 
@@ -85,6 +95,9 @@ namespace theoretica {
 
 	/// Integrate numerically a differential equation in one unknown
 	/// using Heun's method.
+	/// @param f A function which computes the derivative of the state
+	/// @param s The current state of the ODE
+	/// @param h The real step size
 	ode_state<1> integrate_ode_heun(real(*f)(real, real), ode_state<1> s, real h) {
 
 		const real y_p = s.y + h * f(s.t, s.y);
@@ -96,6 +109,9 @@ namespace theoretica {
 
 	/// Integrate numerically a differential equation in N unknowns
 	/// using Heun's method.
+	/// @param f A function which computes the derivative of the state
+	/// @param s The current state of the ODE
+	/// @param h The real step size
 	template<unsigned int N>
 	ode_state<N> integrate_ode_heun(vec<N>(*f)(real, vec<N>), ode_state<N> s, real h) {
 
@@ -108,6 +124,9 @@ namespace theoretica {
 
 	/// Integrate numerically a differential equation in one unknown
 	/// using Runge-Kutta's method of fourth order
+	/// @param f A function which computes the derivative of the state
+	/// @param s The current state of the ODE
+	/// @param h The real step size
 	ode_state<1> integrate_ode_rk4(real(*f)(real, real), ode_state<1> s, real h) {
 
 		const real k1 = f(s.t, s.y);
@@ -121,6 +140,9 @@ namespace theoretica {
 
 	/// Integrate numerically a differential equation in N unknowns
 	/// using Runge-Kutta's method of fourth order
+	/// @param f A function which computes the derivative of the state
+	/// @param s The current state of the ODE
+	/// @param h The real step size
 	template<unsigned int N>
 	ode_state<N> integrate_ode_rk4(vec<N>(*f)(real, vec<N>), ode_state<N> s, real h) {
 
@@ -135,6 +157,9 @@ namespace theoretica {
 
 	/// Integrate numerically a differential equation in one unknown
 	/// using Kutta's 3/8 rule method
+	/// @param f A function which computes the derivative of the state
+	/// @param s The current state of the ODE
+	/// @param h The real step size
 	ode_state<1> integrate_ode_k38(real(*f)(real, real), ode_state<1> s, real h) {
 
 		const real k1 = f(s.t, s.y);
@@ -148,6 +173,9 @@ namespace theoretica {
 
 	/// Integrate numerically a differential equation in N unknowns
 	/// using Kutta's 3/8 rule method
+	/// @param f A function which computes the derivative of the state
+	/// @param s The current state of the ODE
+	/// @param h The real step size
 	template<unsigned int N>
 	ode_state<N> integrate_ode_k38(vec<N>(*f)(real, vec<N>), ode_state<N> s, real h) {
 
@@ -157,6 +185,56 @@ namespace theoretica {
 		const vec<N> k4 = f(s.t + h, s.y + h * (k1 - k2 + k1));
 
 		return ode_state<N>(s.t + h, s.y + (k1 + 3 * k2 + 3 * k3 + k4) * h / 8.0);
+	}
+
+
+	/// Integrate numerically a differential equation in 1 unknown
+	// using the Adams-Bashforth linear multistep method of the second order
+	ode_state<1> integrate_ode_adams(real(*f)(real, real),
+		ode_state<1> s0, ode_state<1> s1, real h) {
+
+		return ode_state<1>(
+			s1.t + h,
+			s1.y + h * (3. * f(s1.t, s1.y) / 2. - f(s0.t, s0.y) / 2.));
+	}
+
+
+	/// Integrate numerically a differential equation in N unknowns
+	// using the Adams-Bashforth linear multistep method of the second order
+	template<unsigned int N>
+	ode_state<N> integrate_ode_adams(vec<N>(*f)(real, vec<N>),
+		ode_state<N> s0, ode_state<N> s1, real h) {
+
+		return ode_state<N>(
+			s1.t + h,
+			s1.y + h * (3. * f(s1.t, s1.y) / 2. - f(s0.t, s0.y) / 2.));
+	}
+
+
+	/// Integrate numerically a differential equation in 1 unknown
+	// using the Adams-Bashforth linear multistep method of third order
+	ode_state<1> integrate_ode_adams3(real(*f)(real, real),
+		ode_state<1> s0, ode_state<1> s1, ode_state<1> s2, real h) {
+
+		return ode_state<1>(
+			s2.t + h,
+			s2.y + h * (23. / 12. * f(s2.t, s2.y)
+				- 4. / 3.0 * f(s1.t, s1.y)
+				+ 3. / 8. * f(s0.t, s0.y)));
+	}
+
+
+	/// Integrate numerically a differential equation in N unknowns
+	// using the Adams-Bashforth linear multistep method of third order
+	template<unsigned int N>
+	ode_state<N> integrate_ode_adams3(vec<N>(*f)(real, vec<N>),
+		ode_state<N> s0, ode_state<N> s1, ode_state<N> s2, real h) {
+
+		return ode_state<N>(
+			s2.t + h,
+			s2.y + h *	(23. / 12. * f(s2.t, s2.y)
+						- 4. / 3.0 * f(s1.t, s1.y)
+						+ 3. / 8. * f(s0.t, s0.y)));
 	}
 
 }
