2d5290ac9c8d5e902d7d7b48136f5ad75ea81a86
Mattia Isgrò <mattiaisgro@users.noreply.github.com>
2024-05-14 14:31:07 +0200

    Generalize functions on datasets

--- src/core/dataset.h
+++ src/core/dataset.h
@@ -26,14 +26,14 @@ namespace theoretica {
 
 	/// Compute the product of a set of values
 	template<typename Vector>
-	inline real product(const Vector& X) {
+	inline auto product(const Vector& X) {
 
 		if(!X.size()) {
 			TH_MATH_ERROR("product", X.size(), INVALID_ARGUMENT);
 			return nan();
 		}
 
-		real res = 1;
+		auto res = decltype(X[0])(1);
 		for(unsigned int i = 0; i < X.size(); i++)
 			res *= X[i];
 
@@ -43,14 +43,14 @@ namespace theoretica {
 
 	/// Sum the products of two sets of values
 	template<typename Vector>
-	inline real product_sum(const Vector& X, const Vector& Y) {
+	inline auto product_sum(const Vector& X, const Vector& Y) {
 
 		if(X.size() != Y.size()) {
 			TH_MATH_ERROR("product_sum", X.size(), INVALID_ARGUMENT);
 			return nan();
 		}
 
-		real res = 0;
+		auto res = decltype(X[0])(0);
 		for(unsigned int i = 0; i < X.size(); i++)
 			res += X[i] * Y[i];
 
@@ -60,16 +60,16 @@ namespace theoretica {
 
 	/// Sum the products of the squares of two sets of data
 	template<typename Vector>
-	inline real product_sum_squares(const Vector& X, const Vector& Y) {
+	inline auto product_sum_squares(const Vector& X, const Vector& Y) {
 
 		if(X.size() != Y.size()) {
 			TH_MATH_ERROR("product_sum_squares", X.size(), INVALID_ARGUMENT);
 			return nan();
 		}
 
-		real res = 0;
+		auto res = decltype(X[0])(0);
 		for(unsigned int i = 0; i < X.size(); i++)
-			res += square(X[i]) * square(Y[i]);
+			res += (X[i] * X[i]) * (Y[i] * Y[i]);
 
 		return res;
 	}
@@ -77,14 +77,14 @@ namespace theoretica {
 
     /// Sum the products of three sets of values
     template<typename Vector>
-	inline real product_sum(const Vector& X, const Vector& Y, const Vector& Z) {
+	inline auto product_sum(const Vector& X, const Vector& Y, const Vector& Z) {
 
 		if(X.size() != Y.size() || X.size() != Z.size()) {
 			TH_MATH_ERROR("product_sum", X.size(), INVALID_ARGUMENT);
 			return nan();
 		}
 
-		real res = 0;
+		auto res = decltype(X[0])(0);
 		for(unsigned int i = 0; i < X.size(); i++)
 			res += X[i] * Y[i] * Z[i];
 
@@ -94,14 +94,14 @@ namespace theoretica {
 
     /// Sum the quotients of two sets of values
 	template<typename Vector>
-	inline real quotient_sum(const Vector& X, const Vector& Y) {
+	inline auto quotient_sum(const Vector& X, const Vector& Y) {
 
 		if(X.size() != Y.size()) {
 			TH_MATH_ERROR("quotient_sum", X.size(), INVALID_ARGUMENT);
 			return nan();
 		}
 
-		real res = 0;
+		auto res = decltype(X[0])(0);
 		for(unsigned int i = 0; i < X.size(); i++) {
 
 			if(Y[i] == 0) {
@@ -118,10 +118,10 @@ namespace theoretica {
 
     /// Sum the squares of a set of values
     template<typename Vector>
-	inline real sum_squares(const Vector& X) {
+	inline auto sum_squares(const Vector& X) {
 
-		real res = 0;
-		for(unsigned int i = 0; i < X.size(); i++)
+		auto res = X[0] * X[0];
+		for(unsigned int i = 1; i < X.size(); i++)
 			res += X[i] * X[i];
 
 		return res;
@@ -130,10 +130,10 @@ namespace theoretica {
 
     /// Sum together a set of values
 	template<typename Vector>
-	inline real sum(const Vector& X) {
+	inline auto sum(const Vector& X) {
 
-		real res = 0;
-		for(unsigned int i = 0; i < X.size(); i++)
+		auto res = X[0];
+		for(unsigned int i = 1; i < X.size(); i++)
 			res += X[i];
 
 		return res;
@@ -141,7 +141,7 @@ namespace theoretica {
 
 
 	/// Apply a function to a set of values element-wise
-	/// (vectorized evaluation)
+	/// (vectorized evaluation).
 	template<typename Vector, typename Function>
 	inline Vector& apply(Function f, Vector& X) {
 
@@ -153,8 +153,7 @@ namespace theoretica {
 
 
 	/// Get a new vector obtained by applying
-	/// the function element-wise
-	/// (vectorized evaluation)
+	/// the function element-wise (vectorized evaluation).
 	template<typename Vector1, typename Vector2 = Vector1, typename Function>
 	inline Vector2& map(Function f, const Vector1& src, Vector2& dest) {
 
@@ -172,8 +171,7 @@ namespace theoretica {
 
 
 	/// Get a new vector obtained by applying
-	/// the function element-wise
-	/// (vectorized evaluation)
+	/// the function element-wise (vectorized evaluation).
 	template<typename Vector1, typename Vector2 = Vector1, typename Function>
 	inline Vector1 map(Function f, const Vector1& X) {
 
@@ -207,14 +205,14 @@ namespace theoretica {
 
 	/// Finds the maximum value inside a dataset
 	template<typename Vector>
-	inline real max(const Vector& X) {
+	inline auto max(const Vector& X) {
 
 		if(!X.size()) {
 			TH_MATH_ERROR("max", X.size(), INVALID_ARGUMENT);
 			return nan();
 		}
 
-		real curr = X[0];
+		auto curr = X[0];
 
 		for (unsigned int i = 1; i < X.size(); ++i)
 			if(X[i] > curr)
@@ -226,14 +224,14 @@ namespace theoretica {
 
 	/// Finds the minimum value inside a dataset
 	template<typename Vector>
-	inline real min(const Vector& X) {
+	inline auto min(const Vector& X) {
 
 		if(!X.size()) {
 			TH_MATH_ERROR("min", X.size(), INVALID_ARGUMENT);
 			return nan();
 		}
 
-		real curr = X[0];
+		auto curr = X[0];
 
 		for (unsigned int i = 1; i < X.size(); ++i)
 			if(X[i] < curr)
