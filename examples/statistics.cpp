7f9c1cb9b9bd3df8b1c5bd11c89a754953d3c310
Mattia Isgrò <mattiaisgro@users.noreply.github.com>
2024-06-08 12:18:01 +0200

    Fix statistics example

--- examples/statistics.cpp
+++ examples/statistics.cpp
@@ -15,27 +15,27 @@ using namespace th;
 int main() {
 
 
-	std::vector<real> X;
+	vec<real> X;
 
 	// Get the data from standard input
-	std::cout << "Insert dataset (write END to stop):" << std::endl;
-	insert_data(X, "END");
+	println("Insert dataset (empty line to stop):");
+	X = readln();
 
 
 	// You can easily compute statistical functions on the data set
 	// by using functionalities defined in "statistics.h"
-	std::cout << std::endl;
-	std::cout << "N = " << X.size() << std::endl;
-	std::cout << "Mean: " << mean(X) << std::endl;
-	std::cout << "Variance: " << sample_variance(X) << std::endl;
+	println();
+	println("N =", X.size());
+	println("Mean:", mean(X));
+	println("Variance:", sample_variance(X));
 
 	// Many functions with long names have short-hands:
 	// sample_standard_deviation = smpl_stdev
 	// sample_mean_standard_deviation = smpl_stdom
-	std::cout << "Standard Deviation: " << smpl_stdev(X) << std::endl;
-	std::cout << "Mean Standard Deviation: " << smpl_stdom(X) << std::endl;
+	println("Standard Deviation:", smpl_stdev(X));
+	println("Mean Standard Deviation:", smpl_stdom(X));
 	
-	std::cout << "Relative Error: " << sample_standard_relative_error(X) * 100 << "%" << std::endl;
+	println("Relative Error:", sample_standard_relative_error(X) * 100, "%");
 
 	return 0;
 }
