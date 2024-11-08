ff3b48aba1c8ea66f7e7fb48aaca8564cb9dfcd1
mattiaisgro <mattiaisgro@users.noreply.github.com>
2024-11-08 12:00:58 +0000

    Deploying to gh-pages from @ chaotic-society/theoretica@8812bf7c934325dd1b4150ba64cab2e09cca64d1 🚀

--- test__algebra_8cpp.js
+++ test__algebra_8cpp.js
@@ -2,6 +2,8 @@ var test__algebra_8cpp =
 [
     [ "linf_norm", "test__algebra_8cpp.html#aafc8b694483e28f1e783422b0c145354", null ],
     [ "main", "test__algebra_8cpp.html#abf9e6b7e6f15df4b525a2e7705ba3089", null ],
-    [ "rand_mat", "test__algebra_8cpp.html#ad185edd35fcfd8479177d5770ebee1a6", null ],
-    [ "rand_mat_symmetric", "test__algebra_8cpp.html#a634a07923e2017b8e491f444cda5d0bc", null ]
+    [ "mat_estimator", "test__algebra_8cpp.html#a623f155f6974ed3e5ef4f72b0b6d0435", null ],
+    [ "rand_mat", "test__algebra_8cpp.html#a4e8e89494c2065ed05132f2a5762d463", null ],
+    [ "rand_mat_posdef", "test__algebra_8cpp.html#a442658981265a170f03236760bee7fe8", null ],
+    [ "rand_mat_symmetric", "test__algebra_8cpp.html#ad5b65b88a1bfcd1e6652f43d43945eed", null ]
 ];
