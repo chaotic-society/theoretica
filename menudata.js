6e25c5b01f97c0a8ccad573a28214aefc04af202
mattiaisgro <mattiaisgro@users.noreply.github.com>
2025-04-30 13:55:37 +0000

    Deploying to gh-pages from @ chaotic-society/theoretica@9ab45357f8fdad20af0316ebb05527b6d95e65f7 🚀

--- menudata.js
+++ menudata.js
@@ -23,7 +23,7 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Main",url:"index.html"},
+{text:"Main Page",url:"index.html"},
 {text:"Namespaces",url:"namespaces.html",children:[
 {text:"Namespace List",url:"namespaces.html"},
 {text:"Namespace Members",url:"namespacemembers.html",children:[
