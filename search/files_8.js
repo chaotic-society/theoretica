a773fa610d7cd8d15456b6913a9f060dc77da407
mattiaisgro <mattiaisgro@users.noreply.github.com>
2025-05-01 15:13:26 +0000

    Deploying to gh-pages from @ chaotic-society/theoretica@268c25a500a465e432ba459bdc63a6b6a5aacbdf 🚀

--- search/files_8.js
+++ search/files_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['integral_2eh_0',['integral.h',['../integral_8h.html',1,'']]]
+  ['integral_2eh_0',['integral.h',['../integral_8h.html',1,'']]],
+  ['interpolation_2fpolynomial_2eh_1',['polynomial.h',['../interpolation_2polynomial_8h.html',1,'']]]
 ];
