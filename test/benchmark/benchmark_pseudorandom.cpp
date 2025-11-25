56c7fbcadd11a47238cc99b84ba5cc5d36c6c94e
Mattia Isgrò <mattiaisgro@users.noreply.github.com>
2025-11-25 16:57:21 +0100

    Fix and improve benchmarks

--- test/benchmark/benchmark_pseudorandom.cpp
+++ test/benchmark/benchmark_pseudorandom.cpp
@@ -8,23 +8,21 @@ using namespace theoretica;
 
 
 // Sample size for PRNGs
-const size_t N = 1000000;
+const size_t N = 1'000'000;
 
 
 // Benchmark a pseudorandom number generator
-void benchmark_PRNG(benchmark::benchmark_context& ctx, PRNG& g, vec<uint64_t>& v) {
+void benchmark_PRNG(std::string PRNGName, benchmark::benchmark_context& ctx, PRNG& g) {
 
-	auto dummy = []() -> real { return 0; };
-	auto opt = benchmark::benchmark_options<real>(N, 1, dummy);
+	vec<uint64_t> v (N);
+	auto opt = benchmark::benchmark_options<real>(50, 1);
 
-	ctx.benchmark("PRNG::xoshiro",
+	ctx.benchmark("PRNG::" + PRNGName + " (1M)",
 		[&](real x) {
 			for (size_t i = 0; i < N; ++i)
 				v[i] = g();
 			return v[0];
 	}, opt);
-
-	ctx.wait_results();
 }
 
 
@@ -32,13 +30,13 @@ int main(int argc, char const *argv[]) {
 
 	auto ctx = benchmark::make_context("pseudorandom", argc, argv);
 
-	ctx.settings.outputFiles = { "test/benchmark/" };
+	ctx.settings.outputFiles = { "test/benchmark/benchmark_pseudorandom.csv" };
 	ctx.settings.defaultIterations = 10;
 	ctx.settings.defaultRuns = 10;
 
 	// Disable multithreading to avoid segmentation faults
 	// caused by PRNG benchmark
-	//ctx.settings.multithreaded = false;
+	ctx.settings.multithreading = false;
 
 	const uint64_t seed = time(nullptr);
 	PRNG g_xoshiro = PRNG::xoshiro(seed);
@@ -51,13 +49,10 @@ int main(int argc, char const *argv[]) {
 	vec<uint64_t> v = vec<uint64_t>(N);
 
 	// Measure the time taken to generate 1 million numbers
-	benchmark_PRNG(ctx, g_xoshiro, v);
 
-	benchmark_PRNG(ctx, g_wyrand, v);
-
-	benchmark_PRNG(ctx, g_lc, v);
-
-	benchmark_PRNG(ctx, g_splitmix64, v);
-
-	benchmark_PRNG(ctx, g_middlesquare, v);
+	benchmark_PRNG("xoshiro", ctx, g_xoshiro);
+	benchmark_PRNG("wyrand", ctx, g_wyrand);
+	benchmark_PRNG("linear_congruential", ctx, g_lc);
+	benchmark_PRNG("splitmix64", ctx, g_splitmix64);
+	benchmark_PRNG("middlesquare", ctx, g_middlesquare);
 }
