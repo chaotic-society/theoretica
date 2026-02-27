c1a7b975205ee9c7565a9176eda4b3dd16ba36c5
mattiaisgro <mattiaisgro@users.noreply.github.com>
2026-02-27 14:24:01 +0000

    Deploying to gh-pages from @ chaotic-society/theoretica@df28915ba38a331b8adce18d4245dcc71c1790dd 🚀

--- splines_8h.js
+++ splines_8h.js
@@ -4,8 +4,6 @@ var splines_8h =
     [ "theoretica::spline", "classtheoretica_1_1spline.html", "classtheoretica_1_1spline" ],
     [ "bezier", "splines_8h.html#ac9bfc7cddd25fd7dd7eb774e9ea1bef4", null ],
     [ "cubic_bezier", "splines_8h.html#acc018b01eb3df4f133863741a5ed343b", null ],
-    [ "cubic_splines", "splines_8h.html#ae7bf96b1e850f0901d249adbfea8f382", null ],
-    [ "cubic_splines", "splines_8h.html#a99316012fec5500c779ddf8d4114b643", null ],
     [ "invlerp", "splines_8h.html#a7169371ce472198840dea64d46b21ca4", null ],
     [ "invlerp", "splines_8h.html#a7a854543c4d5334c61247aed920937c7", null ],
     [ "lerp", "splines_8h.html#a9ab906f1a292b9290eaeb5b947b34561", null ],
@@ -16,5 +14,7 @@ var splines_8h =
     [ "remap", "splines_8h.html#a6d55941e7aa059d066873e4116c531b0", null ],
     [ "slerp", "splines_8h.html#acfe689c12108304dd74edc6b6e92ff84", null ],
     [ "smootherstep", "splines_8h.html#a423c4bdba84ea280878dcef43b5df206", null ],
-    [ "smoothstep", "splines_8h.html#a75b8a327e65a2d21993a28009828faae", null ]
+    [ "smoothstep", "splines_8h.html#a75b8a327e65a2d21993a28009828faae", null ],
+    [ "splines_cubic", "splines_8h.html#ade4377cc4fef460684fa34287dc39413", null ],
+    [ "splines_cubic", "splines_8h.html#a60fe877040b2f8a88fa5501091d8740f", null ]
 ];
